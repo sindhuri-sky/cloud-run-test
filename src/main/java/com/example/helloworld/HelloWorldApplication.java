0d75bf72123b26406e78c7caeb8901c7e2f0b416
Joanne Nolan <joanne.nolan@sky.uk>
2020-03-05 15:38:41 +0000

    fix filename

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class HelloworldApplication {
+public class HelloWorldApplication {
 
     @Value("${TARGET:World}")
     String target;
@@ -21,6 +21,6 @@ public class HelloworldApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloworldApplication.class, args);
+        SpringApplication.run(HelloWorldApplication.class, args);
     }
 }
