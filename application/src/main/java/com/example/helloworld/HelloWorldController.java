415d208a8dc9d0da652d1c3d9f7217c0012cbd7e
Joanne Nolan <joanne.nolan@sky.uk>
2020-03-09 16:20:13 +0000

    Add missing classes and update cloudbuild

---
+++ application/src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @Value("${TARGET:World}")
+    String target;
+
+    @GetMapping()
+    String hello() {
+
+        return "Hello " + target + " Version 2!";
+    }
+
+}
--- cucumber-tests/runInt.sh
+++ cucumber-tests/runInt.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-
 mvn verify -Dcucumber.options="--strict --tags @int --plugin pretty classpath:features"
